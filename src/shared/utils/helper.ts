39186160bc00c5a9dd72b90f90009c493f20720b
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-02 19:36:23 +0800

    refactor: remove date parser util

--- src/shared/utils/helper.ts
+++ src/shared/utils/helper.ts
@@ -1,12 +1,6 @@
 import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
-import dayjs from 'dayjs'
 
 export function mergeClass(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function parseReadableDate(date: string) {
-  const readableDate = dayjs(date).format('MMM, DD YYYY')
-  return readableDate
 }
