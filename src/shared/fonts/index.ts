29d638a57d7b13a9119aa66bf748ed590ee8230e
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-02 20:45:42 +0800

    refactor: add bricolage font

--- src/shared/fonts/index.ts
+++ src/shared/fonts/index.ts
@@ -1,4 +1,4 @@
-import { Inter, JetBrains_Mono } from 'next/font/google'
+import { Inter, JetBrains_Mono, Bricolage_Grotesque } from 'next/font/google'
 
 const inter = Inter({
   display: 'swap',
@@ -14,7 +14,15 @@ const jetBrainsMono = JetBrains_Mono({
   weight: ['400', '500', '600', '700', '800'],
 })
 
+const bricolageGrotesque = Bricolage_Grotesque({
+  display: 'swap',
+  variable: '--font-bricolage-grotesque',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800'],
+})
+
 export const fonts = {
   inter,
   jetBrainsMono,
+  bricolageGrotesque,
 }
