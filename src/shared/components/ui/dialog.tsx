5da54c4542644defc66d27886d5eed7b7d8ed6df
Nyoman Sunima <nyomansunima@gmail.com>
2025-04-07 21:01:08 +0800

    refactor: change size in mobile

--- src/shared/components/ui/dialog.tsx
+++ src/shared/components/ui/dialog.tsx
@@ -34,7 +34,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={mergeClass(
-        'fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-border bg-background p-1 rounded-2xl duration-300 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-bottom-[48%] data-[state=open]:slide-in-from-bottom-[48%]',
+        'fixed left-[50%] top-[50%] z-50 grid w-[90%] tablet:w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-border bg-background p-1 rounded-2xl duration-300 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-bottom-[48%] data-[state=open]:slide-in-from-bottom-[48%]',
         className,
       )}
       {...props}
@@ -46,7 +46,7 @@ const DialogContent = React.forwardRef<
         </ScrollArea>
         <DialogPrimitive.Close className="flex items-center px-3 py-2 rounded-full gap-2 font-medium font-heading absolute right-4 top-4 bg-secondary text-secondary-foreground cursor-pointer text-sm transition-all duration-300 hover:scale-95">
           Close
-          <i className="fi fi-br-x text-xs" />
+          <i className="fi fi-br-cross-small text-sm" />
         </DialogPrimitive.Close>
       </div>
     </DialogPrimitive.Content>
