33245c624e223c1d172278c15a96bf150ec5d8b4
Nyoman Sunima <nyomansunima@gmail.com>
2025-07-19 18:11:22 +0800

    feat: add eslint config

---
+++ eslint.config.ts
@@ -0,0 +1,22 @@
+import {
+  combine,
+  ignores,
+  imports,
+  javascript,
+  react,
+  typescript,
+} from "@antfu/eslint-config"
+
+export default combine(
+  ignores(["build/**", ".react-router/**"]),
+  javascript(),
+  imports(),
+  typescript(),
+  react({
+    overrides: {
+      "react/no-array-index-key": "off",
+      "react-dom/no-dangerously-set-innerhtml": "off",
+      "react-refresh/only-export-components": "off",
+    },
+  }),
+)
