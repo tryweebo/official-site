c212fb65f8a2a47a7043ea8b58acfa0ba94b6d59
Nyoman Sunima <nyomansunima@gmail.com>
2025-10-15 19:34:43 +0800

    refactor: update the vite config to use tanstack start

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,32 @@
-import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
+import { tanstackStart } from "@tanstack/react-start/plugin/vite"
+import react from "@vitejs/plugin-react"
+import { nitro } from "nitro/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    tanstackStart({
+      srcDirectory: "src/app",
+      prerender: {
+        enabled: true,
+        concurrency: 7,
+        autoSubfolderIndex: true,
+        crawlLinks: true,
+        retryCount: 3,
+      },
+      sitemap: {
+        enabled: true,
+        host: "https://www.tryweebo.site",
+      },
+    }),
+    react(),
+    nitro(),
+  ],
 })
