5334be70a1847b715354a75f9fa8e2d6baacf2ca
Nyoman Sunima <nyomansunima@gmail.com>
2025-10-22 18:37:13 +0800

    chore: remove footer and header

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,8 +1,5 @@
 export * from "./centered-layout";
 export * from "./content";
-export * from "./footer";
-
-export * from "./header";
 export * from "./ui/accordion";
 export * from "./ui/button";
 export * from "./ui/dialog";
